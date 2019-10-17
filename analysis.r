37a16902f78085dbeb3f39d0ea8dc10520db36a8
itamarfiorino <itamarfiorino@gmail.com>
2019-10-17 08:39:43 -0400

    Volcano Plot

--- analysis.r
+++ analysis.r
@@ -1,15 +1,34 @@
 library(ggplot2)
+library(tidyverse)
 
 clusters<- read.csv(file="clusters.csv", header=TRUE, sep=",")
 
-p<-c()
-for (row in 1:nrow(clusters)) {
-    p<-c(p,prop.test(x=c(clusters[row,"Charter.Count"],clusters[row,"Other.Count"]),n=c(clusters[row,"Charter.Total"],clusters[row,"Other.Total"]))$p.value)
-}
-qplot(p,binwidth=0.05)
+clusters <- clusters %>% 
+  rowwise %>%
+  mutate(
+    pvals = prop.test(x=c(Charter.Count,Other.Count),n=c(Charter.Total,Other.Total), alternative="two.sided")$p.value
+  ) %>%
+  mutate(
+    Fold.Change = log2((Charter.Count/Charter.Total)/(Other.Count/Other.Total))
+  )
+
+<<<<<<< HEAD
+clusters$threshold <- clusters$pvals < 0.05 
+ggplot(clusters) +
+  geom_point(aes(x=Fold.Change, y=-log10(pvals), colour=threshold)) + ylim(0, 100) +
+  ggtitle("Volcano Plot") +
+  xlab("log2 Fold Change") + 
+  ylab("-log10 p-value") +
+  #scale_y_continuous(limits = c(0,50)) +
+  theme(legend.position = "none",
+        plot.title = element_text(size = rel(1.5), hjust = 0.5),
+        axis.title = element_text(size = rel(1.25)))  
+=======
+
 
 
 ####
 c(clusters[["Charter.Count"]],clusters[["Other.Count"]])
 chisq.test(c(clusters[["Charter.Count"]],clusters[["Other.Count"]]),n=c(clusters[["Charter.Total"]],clusters[["Other.Total"]]))
 contingency<-data.frame(clusters["Charter.Count"],clusters["Other.Count"])
+>>>>>>> fd3eb022731740e486fbbe1e48bee3c4a785dfb5
