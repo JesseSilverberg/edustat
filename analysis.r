db187341b1702dbfb22c87d7408d47d62a253af6
itamarfiorino <itamarfiorino@gmail.com>
2019-10-17 08:40:59 -0400

    Update analysis.r

--- analysis.r
+++ analysis.r
@@ -12,7 +12,6 @@ clusters <- clusters %>%
     Fold.Change = log2((Charter.Count/Charter.Total)/(Other.Count/Other.Total))
   )
 
-<<<<<<< HEAD
 clusters$threshold <- clusters$pvals < 0.05 
 ggplot(clusters) +
   geom_point(aes(x=Fold.Change, y=-log10(pvals), colour=threshold)) + ylim(0, 100) +
@@ -23,14 +22,3 @@ ggplot(clusters) +
   theme(legend.position = "none",
         plot.title = element_text(size = rel(1.5), hjust = 0.5),
         axis.title = element_text(size = rel(1.25)))  
-=======
-
-
-
-
-
-####
-c(clusters[["Charter.Count"]],clusters[["Other.Count"]])
-chisq.test(c(clusters[["Charter.Count"]],clusters[["Other.Count"]]),n=c(clusters[["Charter.Total"]],clusters[["Other.Total"]]))
-contingency<-data.frame(clusters["Charter.Count"],clusters["Other.Count"])
->>>>>>> fd3eb022731740e486fbbe1e48bee3c4a785dfb5
