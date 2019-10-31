a4ced78ceb2570b2d3cde86fbbee54571fbb6b2c
itamarfiorino <itamarfiorino@gmail.com>
2019-10-31 08:29:24 -0400

    GGPlotly
    added interactive

--- analysis.r
+++ analysis.r
@@ -1,5 +1,6 @@
 library(ggplot2)
 library(tidyverse)
+library(plotly)
 
 clusters<- read.csv(file="clusters.csv", header=TRUE, sep=",")
 
@@ -13,8 +14,8 @@ clusters <- clusters %>%
   )
 
 clusters$threshold <- clusters$pvals < 0.05 
-ggplot(clusters) +
-  geom_point(aes(x=Fold.Change, y=-log10(pvals), colour=threshold))  +xlim(-3,3)+
+k <- ggplot(clusters) +
+  geom_point(aes(x=Fold.Change, y=-log10(pvals), colour=threshold, name=School.Name))  +xlim(-3,3)+
   ggtitle("Volcano Plot") +
   xlab("log2 Fold Change") + 
   ylab("-log10 p-value") +
@@ -22,4 +23,6 @@ ggplot(clusters) +
   theme(legend.position = "none",
         plot.title = element_text(size = rel(1.5), hjust = 0.5),
         axis.title = element_text(size = rel(1.25)))  
+
+ggplotly(k)
 
